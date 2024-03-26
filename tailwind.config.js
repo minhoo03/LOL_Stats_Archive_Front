61fccca90b150c5d6ca9ef6f65a91fe75fe2826a
minhoo03 <minhoo0333@gmail.com>
2024-03-27 02:15:54 +0900

    Feat: tailwind-css 적용 및 yarn berry로 마이그레이션

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  // Tailwind CSS configuration
+  mode: "jit",
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    safeList: [],
+    // https://stackoverflow.com/questions/64557697/tailwindcss-not-working-in-create-react-app
+    // App.tsx의 경우, vue나 js 확장자가 아니었기에 인식을 못하였음.
+    // content: ["./index.html", "./src/**/*.{vue,js}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import tailwindcss from "tailwindcss";
+// import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [react(), tailwindcss()],
+// };
+
+import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
+
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
